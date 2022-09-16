7bbd4c3b3e307033ae58928e0d2a36f3eb5c655c
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-09-16 11:51:01 -0300

    feat: componentizando a aplicação

---
+++ src/components/CreateAdBanner.tsx/index.tsx
@@ -0,0 +1,22 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
+
+export function CreateAdBanner() {
+  return (
+    <div className="pt-1 bg-nlw-gradient rounded-lg overflow-hidden mt-8 w-full flex">
+      <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center w-full">
+        <div>
+          <strong className="text-2xl text-white font-black block">
+            Não encontrou seu duo?
+          </strong>
+          <span className="text-zinc-400 block">
+            Publique um anúncio para encontrar novos players!
+          </span>
+        </div>
+        <button className="py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex item-center gap-2">
+          <MagnifyingGlassPlus size={24} />
+          Publicar anúncio
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/GameBanner/index.tsx
@@ -0,0 +1,20 @@
+interface GameBannerProps {
+  bannerUrl: string;
+  title: string;
+  adsCounter: number;
+}
+
+export function GameBanner({ bannerUrl, title, adsCounter }: GameBannerProps) {
+  return (
+    <a href="" className="relative rounded-lg overflow-hidden">
+      <img src={bannerUrl} />
+
+      <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+        <strong className="font-bold text-white block">{title}</strong>
+        <span className="text-zinc-300 text-sm block">
+          {adsCounter} anúncio(s)
+        </span>
+      </div>
+    </a>
+  );
+}
